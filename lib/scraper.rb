da672b497f92e426a0289d96d964d9b5d6727a80
Nathan Mousa <nathanmousa1@gmail.com>
2019-11-14 16:09:39 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,6 +7,10 @@ class Scraper
   
   def get_page
     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+    
+    doc.css(".post").each do |post|
+      
+    
     title = doc.css(".post").first.css("h2").text
     schedule = doc.css(".post").first.css(".date").text
     description = doc.css(".post").first.css("p").text
